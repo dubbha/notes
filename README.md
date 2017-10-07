8c544d58cd09533fa82d61f863398b4cdc6d95f1
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2017-10-07 15:54:49 +0300

    fp-arity

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - [jest-mock-default-export](jest-mock-default-export.js)
 - [jest-test-component-will-unmount](jest-test-component-will-unmount.js)
 - [round-half-down](round-half-down.js)
+- [fp-arity](fp-arity.js)
---
+++ fp-arity.js
@@ -0,0 +1,7 @@
+// https://github.com/getify/Functional-Light-JS/blob/master/ch2.md#counting-inputs
+
+function foo(x,y,z) {
+	return true;
+}
+
+console.log(foo.length); // arity = 3 - foo() expects three arguments, because it has three declared parameters
