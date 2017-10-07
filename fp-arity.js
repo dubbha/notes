6485cb0fbcf22ebad495c773295aade90c7b26de
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2017-10-07 15:58:28 +0300

    fp-arity

--- fp-arity.js
+++ fp-arity.js
@@ -1,8 +1,10 @@
-// https://github.com/getify/Functional-Light-JS/blob/master/ch2.md#counting-inputs
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/length
-
 function foo(x,y,z) {
 	return true;
 }
 
 console.log(foo.length); // arity = 3 - foo() expects three arguments, because it has three declared parameters
+
+/*
+https://github.com/getify/Functional-Light-JS/blob/master/ch2.md#counting-inputs
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/length
+*/
