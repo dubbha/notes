2536cea8b8d283dd8b15c82b9f890763b6930e31
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2017-10-07 16:17:05 +0300

    fp-arity

--- fp-arity.js
+++ fp-arity.js
@@ -4,6 +4,11 @@ function foo(x,y,z) {
 
 console.log(foo.length); // arity = 3 - foo() expects three arguments, because it has three declared parameters
 
+// This number excludes the rest parameter and only includes parameters before the first one with a default value.
+(function f(x, y=5) {}).length;        // 1
+(function f(x, ...rest) {}).length;    // 1
+(function f({ x, y }) {}).length;      // 1
+
 /*
 https://github.com/getify/Functional-Light-JS/blob/master/ch2.md#counting-inputs
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/length
