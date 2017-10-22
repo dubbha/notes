78815211ccaa32f1374918fa25d41b389d344f53
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2017-10-22 16:44:35 +0300

    string-repeated-substring-pattern

--- string-repeated-substring-pattern.js
+++ string-repeated-substring-pattern.js
@@ -1,5 +1,4 @@
 /**
- * https://leetcode.com/problems/repeated-substring-pattern/description/
  * Given a non-empty string check if it can be constructed by taking a substring
  * of it and appending multiple copies of the substring together.
  *
@@ -11,5 +10,6 @@ const repeatedSubstringPattern = (s) => (s+s).slice(1, -1).indexOf(s) > -1;
 
 
 /*
+https://leetcode.com/problems/repeated-substring-pattern/description/
 https://discuss.leetcode.com/topic/68206/easy-python-solution-with-explaination
 */
