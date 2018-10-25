ba867c9381be40b014034a0581e7062877fb3d08
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2018-10-26 01:13:43 +0300

    psp-frequency-counter-anagram

--- psp-frequency-counter-anagram.js
+++ psp-frequency-counter-anagram.js
@@ -1,6 +1,7 @@
 // O(3n) ~ O(n)
-
 const validAnagram = (str, str2) => {
+  if (str.length !== str2.length) return false;
+
   const freqs = charFreqs(str);
   const freqs2 = charFreqs(str2);
 
@@ -14,3 +15,23 @@ const charFreqs = str => str.split('').reduce((acc, cur) => {
 
 validAnagram('iceman', 'cinema');    // true
 // validAnagram('aaz', 'zza');          // false
+
+
+
+// A smarter solution, less concise, but less looping
+// O(2n) ~ O(n)
+const validAnagram = (str, str2) => {
+  if (str.length !== str2.length) return false;
+
+  const freqs = str.split('').reduce((acc, cur) => {
+    acc[cur] = acc[cur] ? ++acc[cur] : 1;
+    return acc;
+  }, {});
+
+  for (let c of str2) {
+    if (!freqs[c]) return false;
+    freqs[c] -= 1;
+  }
+  return true;
+}
+
